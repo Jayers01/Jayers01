c65b7203ca0ae131feac3b45b4f3fc4f57013d9a
Jayers01 <96100872+Jayers01@users.noreply.github.com>
2023-02-28 13:16:44 -0500

    Initial upload of resume website

---
+++ resume_website/interacatbility.js
@@ -0,0 +1,9 @@
+// Resume website
+// Joshua Ayers
+// Email: josh.ayers@gmail.com
+
+const headerCard = document.querySelector('.head');
+
+window.addEventListener('load', () => {
+  head.classList.add('animate__animated', 'animate__fadeInDown');
+});
